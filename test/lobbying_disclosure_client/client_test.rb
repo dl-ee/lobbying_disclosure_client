5364014cee4b2d23363cd1c7e5d3893edb3070fb
Daniel Lee <dlee148@ucla.edu>
2024-08-26 22:16:53 -0600

    Refactor tests to not do so much nasty mocking

--- test/lobbying_disclosure_client/client_test.rb
+++ test/lobbying_disclosure_client/client_test.rb
@@ -4,36 +4,18 @@
 require 'test_helper'
 
 class ClientTest < Minitest::Test
-  def teardown
-    LobbyingDisclosureClient.api_key = nil
-  end
-
   def test_auth_when_no_api_key
-    Faraday.stub(:new, proc { |*args, &_block|
-      headers = args[0][:headers]
+    cassette = YAML.load_file('test/cassettes/v1_anonymous.yml')
+    api_call = cassette['http_interactions'].first
 
-      assert_empty(headers)
-      Faraday.default_connection
-    }) do
-      VCR.use_cassette('/v1') do
-        LobbyingDisclosureClient::Client.new.get('/v1/')
-      end
-    end
+    assert_nil(api_call.dig('request', 'headers', 'Authorization'))
   end
 
   def test_auth_with_api_key
-    api_key = SecureRandom.uuid
-    LobbyingDisclosureClient.api_key = api_key
+    cassette = YAML.load_file('test/cassettes/v1_authorized.yml')
+    api_call = cassette['http_interactions'].first
+    authorization_header = api_call.dig('request', 'headers', 'Authorization').first
 
-    Faraday.stub(:new, proc { |*args, &_block|
-      headers = args[0][:headers]
-
-      assert_equal(headers, { 'Authorization' => "Token #{api_key}" })
-      Faraday.default_connection
-    }) do
-      VCR.use_cassette('/v1') do
-        LobbyingDisclosureClient::Client.new.get('/v1/')
-      end
-    end
+    assert_match(/^Token \w+$/, authorization_header)
   end
 end
--- test/lobbying_disclosure_client/v1_test.rb
+++ test/lobbying_disclosure_client/v1_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 
 class V1Test < Minitest::Test
   def test_route
-    VCR.use_cassette('/v1') do
+    VCR.use_cassette('/v1_anonymous') do
       response = LobbyingDisclosureClient::V1.call
 
       assert_instance_of(Hash, response)
