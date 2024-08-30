1fae88f53b010600116871eaf59f40d2cf9e470f
Daniel Lee <dlee148@ucla.edu>
2024-08-30 12:33:18 -0600

    Relocate login cassette more sensibly

--- test/lobbying_disclosure_client/auth/login_test.rb
+++ test/lobbying_disclosure_client/auth/login_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 
 class LoginTest < Minitest::Test
   def test_login
-    VCR.use_cassette('/login') do
+    VCR.use_cassette('/auth/login') do
       output = LobbyingDisclosureClient::Auth::Login.call(
         input: LobbyingDisclosureClient::Auth::Login::Input.new(
           username: 'user',
